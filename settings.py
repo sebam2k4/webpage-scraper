5bc583cd2efff1cf29db437a2245e8f061748836
sebam2k4 <sebam2k4@gmail.com>
2017-11-23 15:25:49 +0000

    updated database info

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
-DBS_NAME = 'test'
-COLLECTION_NAME = 'test_collection'
+DBS_NAME = 'client-database'
+COLLECTION_NAME = 'webpage_data'
 CSV_FILE_NAME = 'urls.csv'
